59a16aa37cf2e26b1901deb7820a6c5934061397
BuildTools <unconfigured@null.spigotmc.org>
2023-10-31 21:57:43 +0200

    organized

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var typed = new Typed(".multi-text", {
-    strings: ["Full-stack developer", "Discord developer", "Software engineer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-})
+  strings: ["Full-stack developer", "Discord developer", "Software engineer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
